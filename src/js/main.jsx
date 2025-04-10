54cfc945c9f96e6f656360216b586cc444a50b02
Deimian Vásquez <deimianvasquez@gmail.com>
2025-04-10 01:30:27 +0000

    clase numero 1

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,10 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+// import Home from './components/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
+  
+  <h1 className="text-danger">hola</h1>
+
 )
